a0d76ed2f0a707e8daa2eeb9b4c3443d3cf0a328
olegpl1993 <olegpl1993@gmail.com>
2023-03-14 01:25:46 +0200

    feat: routing main about 404 pages

---
+++ src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,14 @@
+import './AboutUs.css';
+import React from 'react';
+
+class AboutUs extends React.Component {
+  render() {
+    return (
+      <div className="aboutUs">
+        <div className="header">About Us</div>
+      </div>
+    );
+  }
+}
+
+export default AboutUs;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,14 @@
+import './Main.css';
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="header">Main page</div>
+      </div>
+    );
+  }
+}
+
+export default Main;
