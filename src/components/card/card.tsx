8861d4b0062fccc02335dc154f7c24aa0d757b6b
olegpl1993 <olegpl1993@gmail.com>
2023-03-26 21:23:55 +0300

    refactor: class component cart to function

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,23 +2,26 @@ import './card.css';
 import React from 'react';
 import { Product } from 'types';
 
-class Card extends React.Component<Product> {
-  render() {
-    return (
-      <div className="card">
-        <div className="title">{this.props.product.title}</div>
-        <div className="row">Category: {this.props.product.category}</div>
-        <div className="row">Brand: {this.props.product.brand}</div>
-        <div className="row">Price: {this.props.product.price}</div>
-        <div className="row">Rating: {this.props.product.rating}</div>
-        <div className="row">Stock: {this.props.product.stock}</div>
-        <div className="row">{this.props.product.description}</div>
-        <div className="imgBox">
-          <img src={this.props.product.thumbnail} className="img" alt="productImg" />
-        </div>
+interface Props {
+  product: Product;
+}
+
+function Card(props: Props) {
+  const { product } = props;
+  return (
+    <div className="card">
+      <div className="title">{product.title}</div>
+      <div className="row">Category: {product.category}</div>
+      <div className="row">Brand: {product.brand}</div>
+      <div className="row">Price: {product.price}</div>
+      <div className="row">Rating: {product.rating}</div>
+      <div className="row">Stock: {product.stock}</div>
+      <div className="row">{product.description}</div>
+      <div className="imgBox">
+        <img src={product.thumbnail} className="img" alt="productImg" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Card;
--- src/types.ts
+++ src/types.ts
@@ -9,17 +9,15 @@ export interface FormData {
 }
 
 export interface Product {
-  product: {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    rating: number;
-    stock: number;
-    brand: string;
-    category: string;
-    thumbnail: string;
-    images: string[];
-  };
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
 }
