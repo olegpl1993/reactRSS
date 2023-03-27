f0cdbffe338d499a4f219eac8d793346c2dd801c
olegpl1993 <olegpl1993@gmail.com>
2023-03-27 23:26:08 +0300

    refactor: rewrite tags at form use formSubmit, formReset

--- src/components/formCard/formCard.test.tsx
+++ src/components/formCard/formCard.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
 import FormCard from './formCard';
 
 const formData = {
