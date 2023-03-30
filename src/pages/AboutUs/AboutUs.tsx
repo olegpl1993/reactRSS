a0408ab5796d618892fc759e0ae527a4b54eec1a
olegpl1993 <olegpl1993@gmail.com>
2023-03-31 00:29:17 +0300

    feat: change AboutUs to function component

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,16 +1,36 @@
 import './AboutUs.css';
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Header from '../Header/Header';
 
-class AboutUs extends React.Component {
-  render() {
-    return (
-      <div className="aboutUs">
-        <Header pageName={'About Us'} />
-        <div className="header">About Us</div>
-      </div>
-    );
-  }
+function AboutUs() {
+  const [testState, setTestState] = useState('Test String');
+
+  const [changeState, setChangeState] = useState(true);
+  console.log('changeState: ', changeState);
+
+  useEffect(() => {
+    console.log('UseEffect');
+    setTestState('UseEffect');
+  }, []);
+
+  useMemo(() => {
+    console.log('UseMemo');
+    setTestState('UseMemo');
+  }, []);
+
+  const changeSet = () => {
+    setChangeState(changeState ? false : true);
+  };
+
+  return (
+    <div className="aboutUs">
+      <Header pageName={'About Us'} />
+      <button className="btn" onClick={changeSet}>
+        BUTTON
+      </button>
+      <div className="testString">{testState}</div>
+    </div>
+  );
 }
 
 export default AboutUs;
