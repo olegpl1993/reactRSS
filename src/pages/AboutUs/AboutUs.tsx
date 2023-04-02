dbde4ee5902083e405666852a43493f935a9c676
olegpl1993 <olegpl1993@gmail.com>
2023-04-02 18:29:25 +0300

    feat: usememo for optimization

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,6 +1,12 @@
+import LoadingSpinner from '../../components/Spinner/LoadingSpinner';
 import { Context } from '../../App';
 import './AboutUs.css';
 import React, { useContext, useEffect, useMemo, useState } from 'react';
+
+const testFunction = (testState: number) => {
+  for (let i = 0; i < 99999999; i++) testState += 1;
+  return testState;
+};
 
 function AboutUs() {
   const { setPageState } = useContext(Context);
@@ -8,30 +14,36 @@ function AboutUs() {
     setPageState('About Us');
   }, [setPageState]);
 
-  const [testState, setTestState] = useState('Test String');
+  const [testState1, setTestState1] = useState(0);
+  const [testState2, setTestState2] = useState(0);
+  const changeState1 = () => setTestState1(testState1 + 1);
+  const changeState2 = () => setTestState2(testState2 + 1);
 
-  const [changeState, setChangeState] = useState(true);
+  const testValue = useMemo(() => {
+    console.log('useMemo');
+    return testFunction(testState1);
+  }, [testState1]);
 
-  useEffect(() => {
-    setTestState('UseEffect');
-  }, []);
-
-  useMemo(() => {
-    setTestState('UseMemo');
-  }, []);
-
-  const changeSet = () => {
-    setChangeState(changeState ? false : true);
-  };
+  console.log('render');
 
   return (
     <div className="aboutUs">
-      <button className="btn" onClick={changeSet}>
-        BUTTON
+      <button className="btn" onClick={changeState1}>
+        Plus State1
+      </button>
+      <button className="btn" onClick={changeState2}>
+        Plus State2
       </button>
       <div className="testString">
-        {testState} {changeState ? 'true' : 'false'}
+        {'state1'} {testState1.toString()}
       </div>
+      <div className="testString">
+        {'state2'} {testState2.toString()}
+      </div>
+      <div className="testString">
+        {'testValue'} {testValue}
+      </div>
+      <LoadingSpinner />
     </div>
   );
 }
