5b2ee8691b6709e1d009c604865fcbfeaa148a88
olegpl1993 <olegpl1993@gmail.com>
2023-04-29 02:51:59 +0300

    fix: browserRouter for unittest

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -2,17 +2,26 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import React from 'react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App', () => {
   it('App rendering', () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
     expect(screen.getByText('Main')).toBeInTheDocument();
     expect(screen.getByText(/About Us/i)).toBeInTheDocument();
     expect(screen.getByText(/Forms/i)).toBeInTheDocument();
     expect(screen.getByText(/Main page/i)).toBeInTheDocument();
   });
   it('App routing', () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
     expect(screen.getByText(/Main page/i)).toBeInTheDocument();
     fireEvent.click(screen.getByText(/About Us/i));
     expect(screen.getByText(/AboutUs page/i)).toBeInTheDocument();
