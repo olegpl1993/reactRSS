ad6463fc7b4ee4113adbc97deabb50b2e41686c7
olegpl1993 <olegpl1993@gmail.com>
2023-04-29 01:40:45 +0300

    feat: e2e test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'cypress';
+import coverage from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  video: false,
   e2e: {
     baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      coverage(on, config);
+      return config;
+    },
   },
 });
